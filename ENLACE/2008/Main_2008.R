3ce18a3bfd3240992b62c5f49db931d551cafd0a
SC187 <11048067+C1587S@users.noreply.github.com>
2019-11-01 12:37:06 -0600

    2008 sin loops infinitos, faltan reintentos

--- ENLACE/2008/Main_2008.R
+++ ENLACE/2008/Main_2008.R
@@ -13,11 +13,13 @@ if(Sys.info()["user"]=="mauricio"){
 } 
 if(Sys.info()["user"]=="c1587s") {
   setwd("/Users/c1587s/Dropbox/Webscrape_Puntajes")
+  dir_do="/Users/c1587s/Documents/GitHub/RScraping/ENLACE/2008/"
 }
 
 
 source(paste0(dir_do,"prelims_2008.R"))
-# Scraping starts
 source(paste0(dir_do,"Scrape_2008.R"))
+# Scraping starts
+retry(Scrape_2008())
 #tryCatch({source(paste0(dir_do,"Scrape_2008.R"))}, silent=FALSE,error=send_error_email())
 #send_noerror_email() # send email if no error occurs
